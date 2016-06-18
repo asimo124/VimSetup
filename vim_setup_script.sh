d58b5e94e32f6bced049770bfddf3c0ba546f710
alex hawley <asimo124@yahoo.com>
2016-06-18 15:30:39 -0500

    2016-06-18 no need for downoad vimrc

--- vim_setup_script.sh
+++ vim_setup_script.sh
@@ -1,9 +1,4 @@
 
-apt-get install git
-
-git clone http://github.com/asimo124/VimSetup.vim ~/
-mv ~/VimSetup/.vimrc ~
-rm -rf ~/VimSetup
 
 mkdir -p ~/.vim/autoload ~/.vim/bundle && \
 curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
